66b8dff3749ccf7abc9fb5867089a6f73771db0c
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-05-08 14:57:49 -0400

    test_file_management.m: Fixed path

--- tests/test_file_management.m
+++ tests/test_file_management.m
@@ -1,12 +1,9 @@
 % Test functions dealing with file management, copy, dicom conversion, etc.
 
-% TODO: add this path only once before running MOxUnit, not in all tests.
-addpath(genpath('../'));
-
 function test_suite=test_file_management
     test_functions=localfunctions();
     initTestSuite;
 
 function test_dicomsorts
-    sortdicoms('dicom_unsorted/', 'dicom_sorted/');
-    assert(isfile('dicom_sorted/06_a_gre_DYNshim/echo_11.5/acdc_95p-HC7;NC1,2-0001-0001.dcm'));
+    sortdicoms('data_testing/dicom_unsorted/', 'data_testing/dicom_sorted/', 0);
+    assert(isfile('data_testing/dicom_sorted/06_a_gre_DYNshim/echo_11.5/acdc_95p-HC7;NC1,2-0001-0001.dcm'));
