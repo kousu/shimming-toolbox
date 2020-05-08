ec245a0d3f90b608035d7cceb45498861f036d22
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-05-08 15:46:23 -0400

    main.yml: Added path

--- tests/test_file_management.m
+++ tests/test_file_management.m
@@ -2,9 +2,8 @@
 
 function test_suite=test_file_management
     test_functions=localfunctions();
-    addpath(genpath('../'))
     initTestSuite;
 
 function test_dicomsorts
-    sortdicoms('data_testing/dicom_unsorted/', 'data_testing/dicom_sorted/', 0);
+    sortdicoms('data_testing/dicom_unsorted/', 'data_testing/dicom_sorted/');
     assert(isfile('data_testing/dicom_sorted/06_a_gre_DYNshim/echo_11.5/acdc_95p-HC7;NC1,2-0001-0001.dcm'));
